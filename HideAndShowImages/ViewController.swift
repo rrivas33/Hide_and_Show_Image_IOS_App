3bff546dd4e10b0ab847e2cc70d8c4c859bc6ce0
raul rivas <Raul@rauls-MacBook-Pro-2.local>
2016-02-01 13:23:57 -0800

    Added files for HideAndShowImages

--- HideAndShowImages/ViewController.swift
+++ HideAndShowImages/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Buttons
+    @IBOutlet weak var redBombButton: UIButton!
+    @IBOutlet weak var blueBombButton: UIButton!
+    
+    //Images
+    @IBOutlet weak var redBombImage: UIImageView!
+    @IBOutlet weak var blueBombImage: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showRedBomb(sender: AnyObject) {
+        
+        if(redBombImage.hidden)
+        {
+            redBombImage.hidden = false
+            redBombButton.setTitle("Hide Red Bomb", forState: UIControlState.Normal)
+        }
+        else
+        {
+            redBombImage.hidden = true
+            redBombButton.setTitle("Show Red Bomb", forState: UIControlState.Normal)
+            
+        }
+        
+    }
 
+    @IBAction func showBlueBomb(sender: AnyObject) {
+        
+        if(blueBombImage.hidden)
+        {
+            blueBombImage.hidden = false
+            blueBombButton.setTitle("Hide Blue Bomb", forState: UIControlState.Normal)
+        }
+        else
+        {
+            blueBombImage.hidden = true
+            blueBombButton.setTitle("Show Blue Bomb", forState: UIControlState.Normal)
+        }
+    }
 }
 
